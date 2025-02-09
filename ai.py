f4ab1db3be6b4e46720e3d30a2f4dd0ead12856e
lemdaghi <ibrahim.lemdaghrialaoui@etu.toulouse-inp.fr>
2025-02-09 17:31:47 +0100

    Add simple IA + managing undo

---
+++ ai.py
@@ -0,0 +1,25 @@
+import random
+
+class AIPlayer:
+    def __init__(self, board, color):
+        self.board = board
+        self.color = color
+
+    def get_best_move(self):
+        """Retourne un mouvement légal aléatoire pour l'IA."""
+        all_moves = self.get_all_legal_moves()
+        if not all_moves:
+            return None  # Aucun coup possible (Pat ou Mat)
+        
+        return random.choice(all_moves)
+
+    def get_all_legal_moves(self):
+        """Retourne une liste de tous les mouvements légaux pour l'IA."""
+        legal_moves = []
+        for row in self.board.grid:
+            for piece in row:
+                if piece and piece.color == self.color:
+                    for move in piece.get_moves(self.board):
+                        if self.board.check_legal_move(piece, move):
+                            legal_moves.append((piece, move))
+        return legal_moves
